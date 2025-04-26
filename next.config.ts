22570a48019a748dfb9999238bcfd9485bcb0303
Happy-Franck <solofonirina35@gmail.com>
2025-04-26 16:19:34 +0300

    Remove dev tool

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // tes autres configs ici...
+  devIndicators: {
+    buildActivity: false,
+  },
 };
 
 export default nextConfig;
